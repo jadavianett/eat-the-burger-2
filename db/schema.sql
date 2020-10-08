fae37a755d7fce61de2449be76e880816fe746bb
Jada Arnett <jada.arnett@gmail.com>
2020-10-08 17:02:14 -0400

    adding imports and exports and orm js files and controller files

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE burgers
+( 
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(255),
+    devoured BOOLEAN,
+    createdAt TIMESTAMP NOT NULL,
+    PRIMARY KEY(id)
+);
+
+
+
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Bacon Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Turkey Burger', false);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Impossible Burger', true);
