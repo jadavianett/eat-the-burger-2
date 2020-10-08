c32a37991c99e80549f1e762b7a3df3144f1698e
Jada Arnett <jada.arnett@gmail.com>
2020-10-08 18:08:10 -0400

    updating connection and adding styles css and js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const mysql = require("mysql");
+const connection = require("./config/connection");
 
 const app = express();
 var PORT = process.env.PORT || 8080;
@@ -13,6 +14,9 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 app.get("/", (req, res)=> {
+    connection.query("SELECT * FROM burgers", (err, data) => {
+        console.table(data);
+    })
     res.render("index");
 })
 
