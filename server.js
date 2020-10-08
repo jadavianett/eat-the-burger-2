a82893e1b8394f010e479675c504b43a302c6f65
Jada Arnett <jada.arnett@gmail.com>
2020-10-08 09:42:44 -0400

    adding views folder, added middleware and get route for idnex

--- server.js
+++ server.js
@@ -3,8 +3,20 @@ const exphbs = require("express-handlebars");
 const mysql = require("mysql");
 
 const app = express();
+var PORT = process.env.PORT || 8080;
 
-app.hget("/api/config", (req, res) => {
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res)=> {
+    res.render("index");
+})
+
+app.get("/api/config", (req, res) => {
     res.json({
         success: true,
     })
